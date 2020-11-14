949f2c6c411eff4fa65fa831e8dc5acf48e168f9
Lucas Mateus <60992605+Luckcastle@users.noreply.github.com>
2020-11-14 15:40:30 -0300

    Add files via upload
    Por favor trocar o link do css no html.

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Noticias.com</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  <div class="container">
+    <header class="cabecalho">
+      <img class="img-logo" src="img/logo.png" alt="logo">
+        <nav class="menu">
+          <h2>Notícias</h2><a href="#" class="exibeNoticias">Exibe notícia</a>
+        </nav>
+        <form>
+          <input type="search" class="campodepesquisa" name="procurar" id="procuraNoticia">
+          <label for="procura" id="procurarNoticia"> <button class="botaoPesquisar" value="Pesquisar">Pesquisar</button></label>
+        </form>
+    </header>
+    <main class="conteudo">
+    <div class="grid-card">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Fulano de tal é vencedor da mega sena</h5>
+            <div class="foto-noticia">
+              <img src="" alt="foto">
+            </div>
+            <p class="card-text"> Fulano de tal mais conhecido como Fulano de tal ganhou uma bolada na mega sena</p>
+            <button class="saiba-mais"><a href="#">Vizualizar</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="grid-card">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Fulano de tal é vencedor da mega sena</h5>
+            <div class="foto-noticia">
+              <img src="" alt="foto">
+            </div>
+            <p class="card-text"> Fulano de tal mais conhecido como Fulano de tal ganhou uma bolada na mega sena</p>
+            <button class="saiba-mais"><a href="#">Vizualizar</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="grid-card">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Fulano de tal é vencedor da mega sena</h5>
+            <div class="foto-noticia">
+              <img src="" alt="foto">
+            </div>
+            <p class="card-text"> Fulano de tal mais conhecido como Fulano de tal ganhou uma bolada na mega sena</p>
+            <button class="saiba-mais"><a href="#">Vizualizar</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="grid-card">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Fulano de tal é vencedor da mega sena</h5>
+            <div class="foto-noticia">
+              <img src="" alt="foto">
+            </div>
+            <p class="card-text"> Fulano de tal mais conhecido como Fulano de tal ganhou uma bolada na mega sena</p>
+            <button class="saiba-mais"><a href="#">Vizualizar</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="grid-card">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Fulano de tal é vencedor da mega sena</h5>
+            <div class="foto-noticia">
+              <img src="" alt="foto">
+            </div>
+            <p class="card-text"> Fulano de tal mais conhecido como Fulano de tal ganhou uma bolada na mega sena</p>
+            <button class="saiba-mais"><a href="#">Vizualizar</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="grid-card">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Fulano de tal é vencedor da mega sena</h5>
+            <div class="foto-noticia">
+              <img src="" alt="foto">
+            </div>
+            <p class="card-text"> Fulano de tal mais conhecido como Fulano de tal ganhou uma bolada na mega sena</p>
+            <button class="saiba-mais"><a href="#">Vizualizar</a></button>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="rodape">rodapé</footer>
+  </div>
+</body>
+</html>
